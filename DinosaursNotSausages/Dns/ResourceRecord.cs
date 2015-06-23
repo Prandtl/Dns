02136ef0a14208ffd6df3cfef4d546e0ebf838ca
Prandtl <prandtl211@gmail.com>
2015-06-24 00:37:45 +0500

    RecourceRecord abstractions : Answer, Authority, Additional

---
+++ DinosaursNotSausages/Dns/ResourceRecord.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DinosaursNotSausages.Dns.Records;
+
+namespace DinosaursNotSausages.Dns
+{
+	class ResourceRecord
+	{
+		public string Name { get; private set; }
+		public Type RrType { get; private set; }
+		public Class RrClass { get; private set; }
+		public uint TTL { get; private set; }
+		public DateTime ArrivalTime { get; private set; }
+
+		public Record Record { get; private set; }
+
+		public ResourceRecord(Reader reader)
+		{
+			Name = reader.ReadDomain();
+			RrType = (Type) reader.ReadUshort();
+			RrClass = (Class) reader.ReadUshort();
+			TTL = reader.ReadUInt();
+			var dataLength = reader.ReadUshort();
+			Record = reader.ReadRecord(RrType,dataLength);
+
+		}
+	}
+
+	class AnswerRR : ResourceRecord
+	{
+		public AnswerRR(Reader reader)
+			: base(reader)
+		{
+		}
+	}
+
+	class AuthorityRR : ResourceRecord
+	{
+		public AuthorityRR(Reader reader)
+			: base(reader)
+		{
+		}
+	}
+
+	class AdditionalRR : ResourceRecord
+	{
+		public AdditionalRR(Reader reader)
+			: base(reader)
+		{
+		}
+	}
+}
